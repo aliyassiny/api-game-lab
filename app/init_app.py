b60f9319c1ac241fc783fa139dbb7affca3c47d7
Ali Yassin <aliyassiny01@gmail.com>
2025-11-16 18:52:41 -0500

    Initial commit

---
+++ app/init_app.py
@@ -0,0 +1,28 @@
+from apis.router import api_router
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from rate_limiting import limiter
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
+
+
+def init_app():
+    app = FastAPI(
+        title="Damn Vulnerable RESTaurant",
+        description="An intentionally vulnerable API service designed for learning and training purposes for ethical hackers, security engineers, and developers.",
+        version="1.0.0",
+        servers=[{"url": "http://localhost:8091", "description": "Local API server"}],
+    )
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origin_regex=".*.(restaurant.com|deliveryservice.com)",
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+    app.state.limiter = limiter
+    app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+    app.include_router(api_router)
+
+    return app
